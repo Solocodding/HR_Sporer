d50efaeae476bd167172abc92b2b9dc981cf1b09
Solocodding <narenderkumar2972002@gmail.com>
2025-05-24 19:31:27 +0530

    Access denied handled

--- middlewares/driverCheck.js
+++ middlewares/driverCheck.js
@@ -3,7 +3,9 @@ const driverCheck = (req, res, next) => {
     if (req.user && req.user.role==='driver' && req.user.isApproved) {
         next();
     } else {
-        res.status(403).send('<script> alert("Access Denied: You are not an approved driver. Wait for admin approval."); history.back();</script>');
+        // res.status(403).send('<script> alert("Access Denied: You are not an approved driver. Wait for admin approval."); history.back();</script>');
+        res.render('auth/login', {message:"Access Denied: You are not an approved driver. Wait for admin approval."});
+        
     }
 };
 
